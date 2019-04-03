550e99b09dbd521119b5e4e784583bd3d87589bf
LDH9494 <48743588+LDH9494@users.noreply.github.com>
2019-04-03 10:55:01 +0900

    Update PushPrint.java
    modifiend in Github

--- Hello world/src/PushPrint.java
+++ Hello world/src/PushPrint.java
@@ -3,6 +3,6 @@ public class PushPrint {
 	public static void main(String[] args) {
 		System.out.println("Add java file");
 		System.out.println("Add java fil2");
-		System.out.println("Hello...");
+		System.out.println("Hello..0.");
 	}
 }
